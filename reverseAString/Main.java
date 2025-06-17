53681709ece8f4993bc9cb2e90b7b2265f4ea230
Aung Myint Myat <amm926616@gmail.com>
2025-06-17 14:30:27 +0630

    reversing a string

--- reverseAString/Main.java
+++ reverseAString/Main.java
@@ -1,5 +1,23 @@
 package reverseAString;
 
 public class Main {
+    public static void main(String[] args) {
+        String name = "AungMyintMyat";
 
+        char[] nameCharArray = name.toCharArray();
+
+        StringBuilder reversedName = new StringBuilder();
+
+        for (int i=0; i < nameCharArray.length; i++) {
+            reversedName.append(nameCharArray[nameCharArray.length -i -1]);
+            System.out.println(nameCharArray[i]);
+        }
+
+        System.out.println(reversedName);
+
+    }
 }
+
+// What you've learnt
+// for loop, advanced for loop.
+// StringBuilder
