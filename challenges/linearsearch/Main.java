b5050021be2e2aa3c87087f6979280be34637d2a
Aung Myint Myat <amm926616@gmail.com>
2025-06-18 16:22:59 +0630

    linear search

--- challenges/linearsearch/Main.java
+++ challenges/linearsearch/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {432, 43124, 80, 2343, 64, 1232, 5456, 326346, 1212625, 123136, 345, 637, 2132};
 
-        System.out.println(doLinearSearch(arr, 326346));
+        System.out.println(doLinearSearch(arr, 4324));
     }
 
     public static int doLinearSearch(int[] arr, int searchItem) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == searchItem) {
-                return i;
+                return i; // return the index where item is found
             }
         }
+        return -1; // item not found - special value for linear search no found
 
-        return arr[arr.length - 1];
     }
 }
